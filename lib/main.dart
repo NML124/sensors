e2f71d0e75a002a8a467c3bd470ce32a91ed19d4
NML124 <nehemielwatumba@gmail.com>
2023-07-07 04:58:36 +0100

    Add setting for magnetometer project

---
+++ lib/main.dart
@@ -0,0 +1,189 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _hasPermissions = false;
+  CompassEvent? _lastRead;
+  DateTime? _lastReadAt;
+  double? heading = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchPermissionStatus();
+    FlutterCompass.events!.listen((event) {
+      setState(() {
+        heading = event.heading;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text('Magnetometer'),
+          centerTitle: true,
+          backgroundColor: Colors.grey.shade900,
+        ),
+        body: Builder(builder: (context) {
+          if (_hasPermissions) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "${heading!.ceil()}°",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Stack(alignment: Alignment.center, children: [
+                    //Image.asset("assets/cadrant.png"),
+                    Transform.rotate(
+                      angle: ((heading ?? 0) * (math.pi / 180) * -1),
+                      child: Image.asset(
+                        "assets/compass.png",
+                        scale: 1.1,
+                      ),
+                    ),
+                  ]),
+                ),
+              ],
+            );
+          } else {
+            return _buildPermissionSheet();
+          }
+        }),
+      ),
+    );
+  }
+
+  Widget _buildManualReader() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: <Widget>[
+          ElevatedButton(
+            child: Text('Read Value'),
+            onPressed: () async {
+              final CompassEvent tmp = await FlutterCompass.events!.first;
+              setState(() {
+                _lastRead = tmp;
+                _lastReadAt = DateTime.now();
+              });
+            },
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    '$_lastRead',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                  Text(
+                    '$_lastReadAt',
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCompass() {
+    return StreamBuilder<CompassEvent>(
+      stream: FlutterCompass.events,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error reading heading: ${snapshot.error}');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        double? direction = snapshot.data!.heading;
+
+        // if direction is null, then device does not support this sensor
+        // show error message
+        if (direction == null)
+          return Center(
+            child: Text("Device does not have sensors !"),
+          );
+
+        return Material(
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Transform.rotate(
+              angle: (direction * (math.pi / 180) * -1),
+              child: Image.asset('assets/compass.png'),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildPermissionSheet() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('Location Permission Required'),
+          ElevatedButton(
+            child: Text('Request Permissions'),
+            onPressed: () {
+              Permission.locationWhenInUse.request().then((ignored) {
+                _fetchPermissionStatus();
+              });
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  void _fetchPermissionStatus() {
+    Permission.locationWhenInUse.status.then((status) {
+      if (mounted) {
+        setState(() => _hasPermissions = status == PermissionStatus.granted);
+      }
+    });
+  }
+}
